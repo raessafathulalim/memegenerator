0a2c6b9c8ec5be782dd84917533fedb5954b278d
Raessa Fathul Alim <raessa_pro@mpotech.net>
2019-06-05 03:39:41 +0800

    Updates

---
+++ precache-manifest.cf96e00d5b027ee4a0d49860d2cd211a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1fac52fda2152f293d78770615d16bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0aa7018515613a3b4f45",
+    "url": "/static/css/main.4ad89e1e.chunk.css"
+  },
+  {
+    "revision": "0f19e2518a3a17b37f23",
+    "url": "/static/js/2.e2adb5f9.chunk.js"
+  },
+  {
+    "revision": "0aa7018515613a3b4f45",
+    "url": "/static/js/main.c4e12c33.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
